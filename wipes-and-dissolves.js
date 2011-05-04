dcc278f2768374647f3e9b4236bd186b838fd202
Carlos R Tirado <pirulee@tremendo.com>
2011-05-04 14:25:37 -0700

    refactor so it all works from single global W_D variable

--- wipes-and-dissolves.js
+++ wipes-and-dissolves.js
@@ -2,50 +2,53 @@
 Canvas Wipes & Dissolves
 Carlos R. Tirado - 2011
 */
-Wipes_and_Dissolves = function(params) {
-	this.canvas = params.canvas; // ph_drawing
+// setup global W_D (for Wipes and Dissolves) object and initialize
+var W_D = {
+	canvas: null,
+	context: null,
+	img1: null,
+	img2: null,
+	vars : { timer: null, loop1: 0, loop2: 0 },
+	source : { x: 0, y: 0, width: 0, height: 0 },
+	destination : { x: 0, y: 0, width: 0, height: 0 },
+}
+// W_D must be setup before effects can be called.
+// need two images (transition one to the other), and a <canvas> element
+W_D.setup = function( params ) {
+	this.canvas = params.canvas;
+	this.img1 = params.img1;
+	this.img2 = params.img2;
 	this.context = this.canvas.getContext('2d');
-	this.img1 = params.img1; // photo
-	this.img2 = params.img2; // drawing_src
-	this.vars = { timer: null, loop1: 0, loop2: 0 };
-	this.source = { x: 0, y: 0, width: 0, height: 0 };
-	this.destination = { x: 0, y: 0, width: 0, height: 0 };
 	this.context.drawImage( this.img2, 0, 0 ); // initialized
-
-	Wipes_and_Dissolves.prototype.reset = function() {
-		if( this.vars.timer ) { clearInterval( this.vars.timer ); }
-		this.source.width = this.img1.width;
-		this.source.height = this.img1.height;
-	};
-
-	/* Effects: */
-	Wipes_and_Dissolves.prototype.fx_random = function() {
-		this.reset();
-		// divvy up canvas in a 20x20 grid (400 squares)
-// this of course should be configurable
-		var grid = new Array();
-		for( i = 0; i < 400; i++ ) { grid[ i ] = i; }
-		this.vars.loop1 = 399; 
-		this.vars.timer = setInterval( function() {
-// problem here relying on global wp instead of 'this'
-// conflict of context within interval, to fix
-			if( 0 <= wp.vars.loop1 ) {
-				r = Math.floor( Math.random() * wp.vars.loop1 );
-				wp.vars.loop2 = grid[ r ];
-				grid[ r ] = grid[ wp.vars.loop1 ];
-				x = ( wp.vars.loop2 % 20 ) * Math.floor( wp.source.width / 20 );
-				y = Math.floor( wp.vars.loop2 / 20 ) * Math.floor( wp.source.height / 20 );
-				w = Math.floor( wp.source.width / 20 );
-				h = Math.floor( wp.source.height / 20 );
-				// one-to-one equivalency between source and destination
-				wp.context.drawImage( wp.img1, x, y, w, h, x, y, w, h );
-				wp.vars.loop1 -= 1;
-// debugging:
-// console.log( 'x: '+x+', y: '+y+' -loop: '+wp.vars.loop2);
-			} else {
-				clearInterval( wp.vars.timer );
-			}
-		}, 1 );
-	};
+}
+W_D.reset = function() {
+	if( this.vars.timer ) { clearInterval( this.vars.timer ); }
+	this.source.width = this.img1.width;
+	this.source.height = this.img1.height;
+}
+/* Effects: */
+W_D.fx_random = function() {
+	this.reset();
+// this of course should be configurable:
+	// divvy up canvas in a 20x20 grid (400 squares)
+	var grid = new Array();
+	for( var i = 0; i < 400; i++ ) { grid[ i ] = i; }
+	this.vars.loop1 = 399;
+	this.vars.timer = setInterval( function() {
+		if( 0 <= W_D.vars.loop1 ) {
+			r = Math.floor( Math.random() * W_D.vars.loop1 );
+			W_D.vars.loop2 = grid[ r ];
+			grid[ r ] = grid[ W_D.vars.loop1 ];
+			x = ( W_D.vars.loop2 % 20 ) * Math.floor( W_D.source.width / 20 );
+			y = Math.floor( W_D.vars.loop2 / 20 ) * Math.floor( W_D.source.height / 20 );
+			w = Math.floor( W_D.source.width / 20 );
+			h = Math.floor( W_D.source.height / 20 );
+			// one-to-one equivalency between source and destination
+			W_D.context.drawImage( W_D.img1, x, y, w, h, x, y, w, h );
+			W_D.vars.loop1 -= 1;
+		} else {
+			clearInterval( W_D.vars.timer );
+		}
+	});
 }
 
